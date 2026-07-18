d0ffd564f9871101da64991741888697febad984
usr40k <usr40k@4d2.org>
2026-07-18 12:05:48 -0400

    some changes

--- llm/start.sh
+++ llm/start.sh
@@ -44,7 +44,7 @@ fi
 # Upgrade pip and install all necessary dependencies
 echo "[*] Checking and updating dependencies..."
 pip install --upgrade pip
-pip install matrix-nio httpx pillow
+pip install matrix-nio httpx pillow bs4
 
 # Run the bot
 echo "[*] Starting the Matrix Based Bot..."
